945ab626e1aa57bc6d364bf7ee4cf834db82ec63
Carla Mendes <carlarsmendes@gmail.com>
2019-04-02 11:46:58 +0100

    updating with correct positioning

--- js/main.js
+++ js/main.js
@@ -2,13 +2,23 @@
 const $player = document.querySelector(".player");
 const $board = document.querySelector(".board-container");
 
+////HERE FOR DEBUGGING AND TESTING
+board = [
+    ["E", "E", "T", "T", "T", "T", "E", "E", "E"],
+    ["T", "T", "T", "E", "E", "T", "T", "T", "T"],
+    ["T", "E", "E", "E", "E", "E", "K", "E", "T"],
+    ["T", "E", "T", "E", "E", "T", "K", "E", "T"],
+    ["T", "E", "B", "E", "B", "T", "E", "P", "T"],     
+    ["T", "T", "T", "T", "T", "T", "T", "T", "T"]
+    ];
+
 
 const $width = $board.offsetWidth
 const $height = $board.offsetHeight
 
 // Some constants
 const NB_OF_TILES_WIDTH = 9
-const NB_OF_TILES_HEIGHT = 7
+const NB_OF_TILES_HEIGHT = 6
 
 const TILE_SIZE_WIDTH = $width / NB_OF_TILES_WIDTH
 const TILE_SIZE_HEIGHT = $height / NB_OF_TILES_HEIGHT
@@ -17,7 +27,7 @@ const TILE_SIZE_HEIGHT = $height / NB_OF_TILES_HEIGHT
 //should I have a new game for each level?
 //should I have a player class? or shoud always be the same? should player be a part of the Game class, or separate?
 
-let game1 = new Game(9,7,2,2);  //this should be the initial sizes and also the position of the player
+let game1 = new Game(9,6,2,2);  //this should be the initial sizes and also the position of the player
 
 window.addEventListener("keydown", function onEvent(event) {
     if (event.key === "ArrowLeft") {
@@ -37,19 +47,19 @@ window.addEventListener("keydown", function onEvent(event) {
 
 
 
-      // T = Tile
+    // T = Tile
     // P = Player
     // B = Beer
     // K = Keg
     // E = Empty
 
 
-///Creating the DIV elements with 'tile' and with correspondant IDs  - How to do this without jQuery?
+///Creating the DIV elements with 'no-tile static' and with correspondant IDs  - How to do this without jQuery?
 
 function structureCreation(){
-for( var i = 0; i < NB_OF_TILES_WIDTH; i++ ) {
-    for( var j = 0; j < NB_OF_TILES_HEIGHT; j++ ) {
-        var newDiv = $( "<div id='" + i + "-" + j + "' class='tile static'></div>" );
+    for( var i = 0; i < NB_OF_TILES_HEIGHT; i++ ) {
+        for( var j = 0; j < NB_OF_TILES_WIDTH; j++ ) {
+        var newDiv = $( "<div id='" + i + "-" + j + "' class='no-tile static'></div>" );
         $( ".board-container" ).append( newDiv );
         // document.getElementById(`"${i}-${j}"`).style.top = `"${i*TILE_SIZE_WIDTH}"`
 
@@ -66,38 +76,65 @@ for( var i = 0; i < NB_OF_TILES_WIDTH; i++ ) {
 
 //tesing wehther manipulating the style was failing because the DIV s had to be created first
 
-function classesAdded(){
-    for( var i = 0; i < NB_OF_TILES_WIDTH; i++ ) {
-        for( var j = 0; j < NB_OF_TILES_HEIGHT; j++ ) {
-            let idIj = `"${i}-${j}"`
-            document.getElementById(idIj).style.top = `"${i*TILE_SIZE_WIDTH}"`}
-        }
-    }
+// function classesAdded(){
+//     for( var i = 0; i < NB_OF_TILES_WIDTH; i++ ) {
+//         for( var j = 0; j < NB_OF_TILES_HEIGHT; j++ ) {
+//             let idIj = `"${i}-${j}"`
+//             document.getElementById(idIj).style.top = `"${i*TILE_SIZE_WIDTH}"`}
+//         }
+//     }
 
 structureCreation();
-classesAdded();
+// classesAdded();
 
 function updateBoard() { //figure out wihout jQuery
-    //     // $( ".tile" ).removeClass( "player" );
-    //     // $( ".tile" ).removeClass( "unvisited" );
-        for( var i = 0; i < $width; i++ ) {
-            for( var j = 0; j < $height; j++ ) {
-    
-        switch (game1.board[i][j])
+    //     // $( ".tile static" ).removeClass( "player" );
+    //     // $( ".tile static" ).removeClass( "unvisited" );
+    for( var i = 0; i < NB_OF_TILES_HEIGHT; i++ ) {
+        for( var j = 0; j < NB_OF_TILES_WIDTH; j++ ) {
+          $( "#" + i + "-" + j  ).removeClass( 'no-tile static' );
+
+        switch (board[i][j])
         {
-            case "P": $( "#" + i + "-" + j ).addClass( "player movable");
+            case "P": $( "#" + i + "-" + j ).addClass("player movable");
             break;
-            case "T": $( "#" + i + "-" + j ).addClass( "tile static" );
+            case "T": $( "#" + i + "-" + j ).addClass("tile static");
             break;
-            case "B": $( "#" + i + "-" + j ).addClass( "beer-empty static" );
+            case "B": $( "#" + i + "-" + j ).addClass("beer-empty static");
             break;
-            case "K": $( "#" + i + "-" + j ).addClass( "keg movable" );
+            case "K": $( "#" + i + "-" + j ).addClass("keg movable");
             break;
-            case "E": $( "#" + i + "-" + j ).addClass( "no-tile static" );
+            case "E": $( "#" + i + "-" + j ).addClass("no-tile static");
             break;
             default: console.log("This tile is outside the game board");   
           }
          }
         }}
        
-     updateBoard();
+updateBoard();
+
+
+     function positionBoard() { //figure out wihout jQuery
+        //     // $( ".tile static" ).removeClass( "player" );
+        //     // $( ".tile static" ).removeClass( "unvisited" );
+        for( var i = 0; i < NB_OF_TILES_HEIGHT; i++ ) {
+            for( var j = 0; j < NB_OF_TILES_WIDTH; j++ ) {
+            
+                $("#" + i + "-" + j  ).css("left" , i*TILE_SIZE_HEIGHT+"px");
+                // $("#" + i + "-" + j  ).css(`'left', '${i*TILE_SIZE_WIDTH}px'`);
+              $( "#" + i + "-" + j  ).css("top", j*TILE_SIZE_WIDTH+"px");
+              $( "#" + i + "-" + j  ).css("width", TILE_SIZE_WIDTH+"px");
+            //   $( "#" + i + "-" + j  ).css("height", TILE_SIZE_HEIGHT+"px");
+            //   $( '#0-5' ).css('top', '300px')
+            //   $('.handle').css('left', '300px');
+
+             }
+            }}
+
+  positionBoard();
+    
+//--------------Here to turn it from empty beer to full beer when the goal is achieved----------
+    //  if(keg.positionX === beer.positionX && keg.positionY === beer.positionY){
+        //change class to "full-beer"
+
+    //  }
